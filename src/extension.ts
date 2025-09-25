b22d6adf30b059aafd59aac0f09308e04387dff9
ryuta <ryuta.bump@gmail.com>
2025-09-25 18:58:44 +0900

    エラー時と保存時のボイス再生追加

--- src/extension.ts
+++ src/extension.ts
@@ -2,19 +2,44 @@ import * as vscode from 'vscode';
 import * as cp from 'child_process';
 import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let isPlaying = false;
+
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('extension.playSound', () => {
-		playSound();
+	let deposable: vscode.Disposable;
+	let soundPath: string;
+
+	// エラーだよ。君なら解決できる。
+	deposable = vscode.languages.onDidChangeDiagnostics((e) => {
+		const diagnostics = vscode.languages.getDiagnostics();
+
+		const hasError = diagnostics.some(([_, diags]) =>
+			diags.some(d => d.severity === vscode.DiagnosticSeverity.Error)
+		);
+
+		if (hasError) {
+			soundPath = path.join(context.extensionPath, 'media', 'sounds', '003_zundamon_error.wav');
+			playSound(soundPath);
+		}
 	});
-	context.subscriptions.push(disposable);
+
+	// お疲れ。いいペースだよ。
+	deposable = vscode.commands.registerCommand('extension.playSound_002_otukare', () => {
+		soundPath = path.join(context.extensionPath, 'media', 'sounds', '002_zundamon_otukare.wav');
+		playSound(soundPath);
+	});
+
+	
+
+	context.subscriptions.push(deposable);
 }
 
-export function playSound() {
-	const soundPath = path.join(__dirname, '..', 'media', 'sounds', '001_zundamon_typing_stop.wav');
-  	cp.exec(`start ${soundPath}`); // Windowsの場合
-}
+function playSound(soundPath: string) {
+	if (isPlaying) return;
 
+	isPlaying = true;
+	cp.exec(`powershell -c (New-Object Media.SoundPlayer "${soundPath}").PlaySync()`, () =>
+		isPlaying = false
+	);
+}
 // This method is called when your extension is deactivated
 export function deactivate() {}
